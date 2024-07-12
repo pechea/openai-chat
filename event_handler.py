a3747d9a8417aab34ef9c8554aa819b529154716
peter chea <peter.chea@gmail.com>
2024-07-11 20:21:25 -0700

    initial commit

---
+++ event_handler.py
@@ -0,0 +1,36 @@
+from typing_extensions import override
+from openai import AssistantEventHandler, OpenAI
+from openai.types.beta.threads import Text, TextDelta
+from openai.types.beta.threads.runs import ToolCall, ToolCallDelta
+
+
+class EventHandler(AssistantEventHandler):
+  def __init__(self, client):
+    super().__init__()
+    self.client = client
+    
+  @override
+  def on_text_created(self, text: Text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+
+  @override
+  def on_text_delta(self, delta: TextDelta, snapshot: Text):
+    print(delta.value, end="", flush=True)
+
+  @override
+  def on_tool_call_created(self, tool_call: ToolCall):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+  @override
+  def on_tool_call_delta(self, delta: ToolCallDelta, snapshot: ToolCall):
+    if delta.type == "code_interpreter" and delta.code_interpreter:
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+  @override
+  def on_message_done(self, message) -> None:
+    print("\n")
